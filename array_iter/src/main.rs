9c0e42d18fa2120be8b9ecd88b27e48076f6d501
arpangreat <arpanthegreat41@gmail.com>
2022-01-08 19:04:47 +0530

    clean ups

--- array_iter/src/main.rs
+++ array_iter/src/main.rs
@@ -4,4 +4,11 @@ fn main() {
     for (x, y) in array.iter() {
         println!("The value of x and y is {} and {}", x, y);
     }
+
+    let mut s1 = String::from("Hello");
+    println!("{:?}", do_stuff(&mut s1));
+}
+
+fn do_stuff(s: &mut String) {
+    s.insert_str(0, "hi, ")
 }
---
+++ array_iter/target/debug/deps/array_iter-003f7d8a75b585e6.d
@@ -0,0 +1,5 @@
+/home/arpangreat/Ultimate-Rust-Crash--Course/array_iter/target/debug/deps/array_iter-003f7d8a75b585e6: src/main.rs
+
+/home/arpangreat/Ultimate-Rust-Crash--Course/array_iter/target/debug/deps/array_iter-003f7d8a75b585e6.d: src/main.rs
+
+src/main.rs:
